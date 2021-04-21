2360f46e0bdc0bed9d52ebf24eba3155c9499e29
filipposonnati <71394034+filipposonnati@users.noreply.github.com>
2021-04-21 12:13:14 +0200

    other pages

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,13 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="")
      */
-    public function white(): Response
+    public function main(): Response
     {
         return $this->render('main/main.html.twig');
+    }
+
+    /**
+     * @Route("/user", name="app_user")
+     * @IsGranted("ROLE_USER")
+     */
+    public function user(): Response
+    {
+        return $this->render('main/user.html.twig');
+    }
+
+    /**
+     * @Route("/admin", name="app_admin")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function admin(): Response
+    {
+        return $this->render('main/admin.html.twig');
     }
 }
