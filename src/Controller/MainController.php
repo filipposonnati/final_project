7be863a193fdc362baf681d67173688c2cdcf4c4
filipposonnati <71394034+filipposonnati@users.noreply.github.com>
2021-05-06 20:39:43 +0200

    messages

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,8 +8,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-use App\Entity\Page; 
-
 class MainController extends AbstractController
 {
     /**
@@ -36,18 +34,5 @@ class MainController extends AbstractController
     public function admin(): Response
     {
         return $this->render('main/admin.html.twig');
-    }
-
-    /**
-     * @Route("/wiki/{title}", name="page")
-     * @IsGranted("ROLE_USER")
-     */
-    public function page(string $title): Response
-    {
-        $page = $this->getDoctrine()->getRepository(Page::class)->findOneBy(['title' => $title]);
-
-        return $this->render('main/page.html.twig', [
-            'page' => $page
-        ]);
     }
 }
---
+++ src/Controller/PageController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+use App\Entity\Page; 
+
+/**
+ * @Route("/wiki", name="wiki_")
+ */
+class PageController extends AbstractController
+{
+    /**
+     * @Route("/{title}", name="page")
+     * @IsGranted("ROLE_USER")
+     */
+    public function page(string $title): Response
+    {
+        $page = $this->getDoctrine()->getRepository(Page::class)->findOneBy(['title' => $title]);
+        $comments = $page->getComments();
+
+        return $this->render('main/page.html.twig', [
+            'page' => $page,
+            'comments' => $comments
+        ]);
+    }
+}
