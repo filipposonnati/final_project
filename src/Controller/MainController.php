5c96a9780c114743111b2f859892d2a94a3f69a4
filipposonnati <filippo.sonnati@protonmail.com>
2021-05-08 10:08:34 +0200

    comment form

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,6 +10,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 use App\Entity\Page;
+use App\Form\CommentType;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/wiki", name="wiki_")
@@ -48,16 +51,38 @@ class MainController extends AbstractController
     /**
      * @Route("/{title}", name="page")
      */
-    public function page(string $title): Response
+    public function page(string $title, Request $request): Response
     {
         $page = $this->getDoctrine()->getRepository(Page::class)->findOneBy(['title' => $title]);
 
         if(!is_null($page)){
             $comments = $page->getComments();
 
+            $comment = new Comment();
+
+            $comment_form = $this->createForm(CommentType::class, $comment);
+
+            $comment_form->handleRequest($request);
+
+            if ($comment_form->isSubmitted() && $comment_form->isValid()) {
+                $comment = $comment_form->getData();
+
+                $comment->setUser($this->getUser());
+                $comment->setPage($page);
+
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($comment);
+                $entityManager->flush();
+    
+                return $this->redirectToRoute('wiki_page', [
+                    'title' => $title
+                ]);
+            }
+
             return $this->render('main/page.html.twig', [
                 'page' => $page,
-                'comments' => $comments
+                'comments' => $comments,
+                'comment_form' => $comment_form->createView()
             ]);
         }
         return $this->render('main/not_found.html.twig');
