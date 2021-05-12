a58f1163bf3398315f0d0c88266b93f2e1c2b97c
filipposonnati <71394034+filipposonnati@users.noreply.github.com>
2021-05-12 15:54:27 +0200

    ricollocazione dei metodi nelle classi

--- src/Controller/BlockController.php
+++ src/Controller/BlockController.php
@@ -3,9 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Block;
+use App\Entity\Comment;
+use App\Form\CommentType;
+use App\Form\TitlePageType;
+use App\Form\TextPageType;
+use App\Form\CodePageType;
+use App\Form\ImagePageType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
@@ -105,6 +114,222 @@ class BlockController extends AbstractController
                     'title' => $block_up->getPage()->getTitle()
                 ]);
             }
+        }
+        throw $this->createNotFoundException('Block not found');
+    }
+
+    /**
+     * @Route("/edit/{id_block}", name="edit")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function edit(int $id_block, Request $request, SluggerInterface $slugger): Response
+    {
+        $block_to_edit = $this->getDoctrine()->getRepository(Block::class)->find($id_block);
+
+        if (!is_null($block_to_edit)) {
+            $page = $block_to_edit->getPage();
+
+            $content_to_reload = $block_to_edit->getContent();
+            $type_to_reload = $block_to_edit->getType();
+
+            /////////////////////////////
+            // eliminazione del blocco //
+            /////////////////////////////
+            $blocks = $this->getDoctrine()->getRepository(Block::class)->getAllAfter($block_to_edit);
+
+            foreach ($blocks as $block) {
+                $block->setPosition($block->getPosition() - 1);
+            }
+
+            $title = $block_to_edit->getPage()->getTitle();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($block_to_edit);
+            $entityManager->flush();
+
+            $comments = $this->getDoctrine()
+                ->getRepository(Comment::class)
+                ->getSortedComments($page->getId());
+
+            $blocks = $this->getDoctrine()
+                ->getRepository(Block::class)
+                ->getSortedBlocks($page->getId());
+
+            $title_blocks = $this->getDoctrine()
+                ->getRepository(Block::class)
+                ->getTitles($page->getId());
+
+            //////////////////////
+            // comment creation //
+            //////////////////////
+            $comment = new Comment();
+            $comment_form = $this->createForm(CommentType::class, $comment, [
+                'action' => $this->generateUrl('wiki_page', [
+                    'title' => $title
+                ])
+            ]);
+            $comment_form->handleRequest($request);
+
+            if ($comment_form->isSubmitted() && $comment_form->isValid()) {
+                $comment = $comment_form->getData();
+
+                $comment->setUser($this->getUser());
+                $comment->setPage($page);
+                $comment->setInsertDateTime(date("Y/m/d H:i"));
+
+                $entityManager->persist($comment);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('wiki_page', [
+                    'title' => $title
+                ]);
+            }
+
+            $lastBlock = $this->getDoctrine()
+                ->getRepository(Block::class)
+                ->findLast($page->getId());
+
+            //////////////////////////
+            // title block creation //
+            //////////////////////////
+            $title_block = new Block();
+            if ($type_to_reload == 'title')
+                $title_block->setContent($content_to_reload);
+            $title_form = $this->createForm(TitlePageType::class, $title_block, [
+                'action' => $this->generateUrl('wiki_page', [
+                    'title' => $title
+                ])
+            ]);
+            $title_form->handleRequest($request);
+
+            if ($title_form->isSubmitted() && $title_form->isValid()) {
+                $title_block = $title_form->getData();
+                $title_block->setType('title');
+                $title_block->setPage($page);
+
+                if ($lastBlock == null) $title_block->setPosition(1);
+                else $title_block->setPosition($lastBlock[0]->getPosition() + 1);
+
+                $entityManager->persist($title_block);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('wiki_page', [
+                    'title' => $title
+                ]);
+            }
+
+            /////////////////////////
+            // text block creation //
+            /////////////////////////
+            $text_block = new Block();
+            if ($type_to_reload == 'text')
+                $text_block->setContent($content_to_reload);
+            $text_form = $this->createForm(TextPageType::class, $text_block, [
+                'action' => $this->generateUrl('wiki_page', [
+                    'title' => $title
+                ])
+            ]);
+            $text_form->handleRequest($request);
+
+            if ($text_form->isSubmitted() && $text_form->isValid()) {
+                $text_block = $text_form->getData();
+                $text_block->setType('text');
+                $text_block->setPage($page);
+
+                if ($lastBlock == null) $text_block->setPosition(1);
+                else $text_block->setPosition($lastBlock[0]->getPosition() + 1);
+
+                $entityManager->persist($text_block);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('wiki_page', [
+                    'title' => $title
+                ]);
+            }
+
+            /////////////////////////
+            // code block creation //
+            /////////////////////////
+            $code_block = new Block();
+            if ($type_to_reload == 'code')
+                $code_block->setContent($content_to_reload);
+            $code_form = $this->createForm(CodePageType::class, $code_block, [
+                'action' => $this->generateUrl('wiki_page', [
+                    'title' => $title
+                ])
+            ]);
+            $code_form->handleRequest($request);
+
+            if ($code_form->isSubmitted() && $code_form->isValid()) {
+                $code_block = $code_form->getData();
+                $code_block->setType('code');
+                $code_block->setPage($page);
+
+                if ($lastBlock == null) $code_block->setPosition(1);
+                else $code_block->setPosition($lastBlock[0]->getPosition() + 1);
+
+                $entityManager->persist($code_block);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('wiki_page', [
+                    'title' => $title
+                ]);
+            }
+
+            //////////////////////////
+            // image block creation //
+            //////////////////////////
+            $image_block = new Block();
+            $image_form = $this->createForm(ImagePageType::class, null, [
+                'action' => $this->generateUrl('wiki_page', [
+                    'title' => $title
+                ])
+            ]);
+            $image_form->handleRequest($request);
+
+            if ($image_form->isSubmitted() && $image_form->isValid()) {
+                $imageFile = $image_form->get('image')->getData();
+
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('image_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    $this->addFlash('error', 'Image cannot be saved.');
+                }
+
+                $image_block->setContent($newFilename);
+                $image_block->setType('image');
+                $image_block->setPage($page);
+
+                if ($lastBlock == null) $image_block->setPosition(1);
+                else $image_block->setPosition($lastBlock[0]->getPosition() + 1);
+
+                $entityManager->persist($image_block);
+                $entityManager->flush();
+                $this->addFlash('success', 'Blog was created!');
+
+                return $this->redirectToRoute('wiki_page', [
+                    'title' => $title
+                ]);
+            }
+
+            return $this->render('main/page.html.twig', [
+                'page' => $page,
+                'title_blocks' => $title_blocks,
+                'blocks' => $blocks,
+                'comments' => $comments,
+                'title_form' => $title_form->createView(),
+                'text_form' => $text_form->createView(),
+                'image_form' => $image_form->createView(),
+                'code_form' => $code_form->createView(),
+                'comment_form' => $comment_form->createView()
+            ]);
         }
         throw $this->createNotFoundException('Block not found');
     }
